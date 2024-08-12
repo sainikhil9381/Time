6fbb5b90246fda61ea6acc45ba5bcc05c40d17ea
sainikhil9381 <sainikhil.akarapu9381@gmail.com>
2024-08-12 23:12:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let hrs=document.getElementById("hours");
+let mins=document.getElementById("minutes");
+let secs=document.getElementById("seconds");
+
+setInterval(()=>{
+let currentTime=new Date();
+hrs.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
+mins.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+secs.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+},1000)
